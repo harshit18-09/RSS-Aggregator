ebee9e14c708132e6e8045669007de1a70118012
HC18 <harshit.chugh18@gmail.com>
2025-10-03 19:37:21 +0530

    initial main setup

---
+++ json.go
@@ -0,0 +1,10 @@
+package main
+
+// import "net/http"
+
+// func respondWithJSON(w, http.RespondWritter, code int, payload interface{}){
+// 	dat, err := json.Marshal(payload)
+// 	if err != nil{
+
+// 	}
+// }
